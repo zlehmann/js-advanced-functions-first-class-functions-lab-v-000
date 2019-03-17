34a4d2b6cdfe45e33c84d1316ddc3b81a57043a0
Zachary Lehmann <zlehmann@gmail.com>
2019-03-17 21:08:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers(drivers) {
-  const function = function() { return drivers.slice(0,2);};
+  const function = function() { return drivers.slice(0,2);}();
 }
