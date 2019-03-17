b4f67d1e31f76b01c1be980ef7cc6ce0bbf3210a
Zachary Lehmann <zlehmann@gmail.com>
2019-03-17 21:05:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solution in this file!
 const firstTwoDrivers = function(drivers) {
-  let result = drivers.slice(0,2);
+  let result = []
+  result = drivers.slice(0,2);
   return result;
 }
